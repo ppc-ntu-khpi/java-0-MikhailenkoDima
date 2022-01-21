39a3bbd986561395f30907fe158bb29fc46df664
MikhailenkoDima <92022943+MikhailenkoDima@users.noreply.github.com>
2022-01-21 09:12:48 +0000

    Сделал строку

--- Solution/task2.md
+++ Solution/task2.md
@@ -3,3 +3,6 @@ Task 2.1:
 
 Task 2.2:
 ![alt-текст](https://raw.githubusercontent.com/ppc-ntu-khpi/java-0-MikhailenkoDima/master/Solution/task%202.2.png)
+
+Task 2.2 que:
+![alt-текст](https://raw.githubusercontent.com/ppc-ntu-khpi/java-0-MikhailenkoDima/master/Solution/task%202.2%20%D0%B8%D0%BC%D1%8F%20%D1%86%D0%B8%D1%82%D0%B0%D1%82%D1%8B.png)
--- src/Shirt.java
+++ src/Shirt.java
@@ -2,7 +2,7 @@ public class Shirt {
   public int shirtID = 0; // стандартне значення номера моделі сорочки
   public String description = "-description required-"; // стандартний опис сорочки
   // коди кольорів: R=червоний, B=синій, G=зелений, U=невідомо
-  public char colorCode = 'U';
+  public String colorCode = "R";
   public double price = 0.0; // стандартна вартість сорочки
   public int quantityInStock = 0; // стандартна кількість на складі
   
